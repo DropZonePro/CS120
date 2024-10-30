7ee5a964b55a1895f061f604f90a929364e7ba9c
DropZonePro <mkeav59@gmail.com>
2024-10-30 12:31:03 -0700

    personal project

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
+
 int main()
 {
-    cout << "Hello World" << endl;
-    exit(0);
-    cout << "Hello World" << endl;
+    string John;
+    string Lee;
+    int var1 = 'John';
+    int var2= 'Lee';
+    int var3 = 100;
+    int var4 = 90;
+    int var5 = 95;
+
+    ofstream ofs;
+
+    ofs.open ("filename.txt");
+
+    ofs << var1 << "\t" << var2 << "\t" << var3 << "\t" << var4 << "\t" << var5;
+
+    ofs.close();
+
 }
